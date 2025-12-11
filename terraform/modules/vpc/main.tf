fc14e69720ebe28f8affa7097079ed04a2b0a11d
burtonjong <burtonjong05@gmail.com>
2025-12-10 21:47:58 -0700

    fix AZ and add to gitignore

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "custom_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.custom_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = var.region
+  availability_zone = "${var.region}a"
 
   tags = {
     Name = "MyVpn"
